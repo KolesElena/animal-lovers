5056a09ce02a8f2549e83a580461b292277b5ad8
Elena Kolesnikova <helena_uni@live.ru>
2022-08-19 15:19:47 +0200

    retrieved users data

--- src/components/ActiveUsers/ActiveUsers.tsx
+++ src/components/ActiveUsers/ActiveUsers.tsx
@@ -1,8 +1,11 @@
+import allUsers from '../../api/users.json';
+
 const ActiveUsers = () => {
+
+    const activeUsers = allUsers.filter((user) => user.isActive);
+    const inactiveUsers = allUsers.filter((user) => !user.isActive);
     
     return (<div>Active users will be shown here</div>)
 };
     
-
-
 export default ActiveUsers;
